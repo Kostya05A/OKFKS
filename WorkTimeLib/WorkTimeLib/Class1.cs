adc3c64170885a5a992b4eef170e25da94052847
Kostya05A <61642989+Kostya05A@users.noreply.github.com>
2024-10-31 07:05:25 +0700

    Add files via upload

---
+++ WorkTimeLib/WorkTimeLib/Class1.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkTimeLib
+{
+    public class Calculation
+    {
+        public static string[] AvailablePeriods(
+            TimeSpan[] startTimes,
+            int[] durations,
+            TimeSpan beginWorkingTime,
+            TimeSpan endWorkingTime,
+            int consultationTime)
+        {
+            return new string[] { };
+        }
+    }
+}
